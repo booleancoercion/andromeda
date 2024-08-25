eef37ca6c0c4ff150a166d93b63a9eea9b621f9d
booleancoercion <booleancoercion@gmail.com>
2024-08-25 21:54:25 +0300

    made /register rejections prettier

--- src/handlers/register.cpp
+++ src/handlers/register.cpp
@@ -21,13 +21,19 @@ bool RegisterGetHandler::matches(const HttpMessage &msg) const {
 HttpResponse RegisterGetHandler::respond(Server &, const HttpMessage &msg) {
     HttpResponse response{};
     if(!is_localhost(msg.get_peer_addr())) {
+        json data{};
+        data["title"] = "Register";
+        data["allowed"] = false;
+        response.body = m_env.render(m_temp, data);
         response.status_code = 403;
+        response.set_content_type(ContentType::TextHtml);
         return response;
     }
 
     if(!msg.get_username().has_value()) {
         json data{};
         data["title"] = "Register";
+        data["allowed"] = true;
         response.body = m_env.render(m_temp, data);
         response.set_content_type(ContentType::TextHtml);
         return response;
@@ -65,27 +71,26 @@ HttpResponse RegisterPostHandler::respond(Server &server,
     auto username_r = msg.get_form_var("username");
     auto password_r = msg.get_form_var("password");
 
+    json data{{"title", "Register"}, {"allowed", true}};
     HttpResponse response{.status_code = 400};
     response.set_content_type(ContentType::TextHtml);
     if(!(username_r.has_value() && password_r.has_value())) {
-        response.body = m_env.render(
-            m_temp, {{"title", "Register"},
-                     {"error", "Please enter a username and a password."}});
+        data["error"] = "Please enter a username and a password.";
+        response.body = m_env.render(m_temp, data);
         return response;
     }
 
     string username{username_r.value()}, password{password_r.value()};
     if(!(is_valid_username(username) && is_valid_password(password))) {
-        response.body =
-            m_env.render(m_temp, {{"title", "Register"},
-                                  {"error", "Invalid username or password."}});
+        data["error"] = "Invalid username or password.";
+        response.body = m_env.render(m_temp, data);
         return response;
     }
 
     auto auth_r = server.get_auth().register_user(username, password);
     if(auth_r.is_err()) {
-        response.body = m_env.render(
-            m_temp, {{"title", "Register"}, {"error", auth_r.get_err()}});
+        data["error"] = auth_r.get_err();
+        response.body = m_env.render(m_temp, data);
         return response;
     }
 
